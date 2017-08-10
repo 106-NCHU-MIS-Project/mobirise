e394f18928b3e6ea511c75bbcf52b894a4c1bf31
Cabao <leo4828@gmail.com>
2017-08-10 23:22:36 +0800

    tmp version

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var APIURL = 'http://data.coa.gov.tw/Service/OpenData/AnimalOpenData.aspx?callback=hello';
+var APIURL = 'https://raw.githubusercontent.com/106-NCHU-MIS-Project/mobirise/gh-pages/Test/Test.json?callback=hello';
 
 var xhr = new XMLHttpRequest();
 xhr.open("GET", APIURL, true);
@@ -9,6 +9,7 @@ xhr.onreadystatechange = function(){
   if (this.readyState === 4 && this.status === 200){
     var data = JSON.parse(this.responseText);
     console.log(data);
+    $('.ininin').text(data[0].animal_kind);
   }
 }
 
